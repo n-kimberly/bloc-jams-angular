34f45e59a037e01e362deaa4a866e777a8e9a276
Kimberly Nguyen <kimberlyvnguyen@gmail.com>
2017-05-20 20:02:30 -0700

    assignment-7-services-pt-2: completed

--- angular/app/scripts/services/SongPlayer.js
+++ angular/app/scripts/services/SongPlayer.js
@@ -1,7 +1,7 @@
 // Create service using factory style declaration to produce instance of SongPlayer.
 
 // Private attributes:      currentSong, currentBuzzObject
-// Private functions:       setSong
+// Private functions:       setSong, playSong
 // Public attributes:
 // Public methods:          SongPlayer.play, SongPlayer.pause
 
@@ -11,10 +11,11 @@
 var SongPlayer,
     currentSong,
     currentBuzzObject,
-    setSong;
+    setSong,
+    playSong;
 
 (function () {
-    "use strict";
+    'use strict';
     function SongPlayer() {
         SongPlayer = {};
 
@@ -34,47 +35,50 @@ var SongPlayer,
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
         * @param {Object} song
         */
-        // setSong switches to new song and takes logical steps to state switching
         setSong = function (song) {
-
             // if a song was defined before, set it now to null
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                song.playing = null;
             }
-
             // Otherwise, set it to the new song
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
-            song = currentSong;
+            currentSong = song;
+        };
+
+        /**
+        * @function playSong
+        * @desc Sets playing boolean to true and plays current song.
+        * @param {Object} song
+        */
+        playSong = function (song) {
+            currentBuzzObject.play();
+            song.playing = true;
         };
 
         SongPlayer.play = function (song) {
-
             // If there is a song playing that isn't the one clicked on, pause it
             // Then set new song as current song and play it.
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
-
+                playSong(song);
             // If the song clicked on was paused, play it
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
 
+        // Pause song
         SongPlayer.pause = function (song) {
             currentBuzzObject.pause();
             song.playing = false;
         };
-
         return SongPlayer;
-
     }
 
     angular
